70814b371c58f41180c3a1b44ede44b95b1184e6
albertoferri <falberto99@gmail.com>
2024-04-09 14:26:56 +0200

    aggiunti altri dettagli nelle card e aggiunto un header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,15 +2,22 @@
 
 @section('content')
 
-<main>
+<main class="row row-cols-4 d-flex flex-wrap my-5 mx-auto" style="width:1200px;">
 
-@foreach ($movies as $movie)
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title">{{ $movie->title }}</h5>
+    @foreach ($movies as $movie)
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">{{ $movie->title }}</h5>
+                <p class="card-text">
+                    <strong>Titolo originale:</strong> {{ $movie->original_title }}<br>
+                    <strong>Nazionalità:</strong> {{ $movie->nationality }}<br>
+                    <strong>Data:</strong> {{ $movie->date }}<br>
+                    <strong>Voto:</strong> {{ $movie->vote }}
+                </p>
+            </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+    
 
 </main>
 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header class="d-flex justify-content-center align-items-center">
+    <img class="" src="{{ Vite::asset('public/img/header-logo.jpg') }}" alt="Logo">
+    <h1 class="">FILM</h1>
+</header>
+
+<style lang="scss">
+    header{
+        
+    }
+</style>
