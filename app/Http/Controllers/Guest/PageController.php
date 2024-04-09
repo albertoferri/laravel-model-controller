20db2582c4da50edf06943bc26dcc2afbff20d82
albertoferri <falberto99@gmail.com>
2024-04-09 11:54:18 +0200

    stampato in pagina i titoli dei film

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie; 
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        // Recupera tutti i film dal database
+        $movies = Movie::all();
+
+        // Passa i film alla view
+        return view('home', ['movies' => $movies]);
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,13 @@
 
 <main>
 
-{{-- contenuto --}}
+@foreach ($movies as $movie)
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">{{ $movie->title }}</h5>
+        </div>
+    </div>
+@endforeach
 
 </main>
 
